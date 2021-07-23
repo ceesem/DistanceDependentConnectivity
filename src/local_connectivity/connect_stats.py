f65c3ea6cca1b1136ec417d1fa9d2050aa51a8b2
Andi Bergeson <amayalon4@gmail.com>
2021-07-22 17:54:21 -0700

    tcreated package from freq used functions, corrected orphan finder

---
+++ src/local_connectivity/connect_stats.py
@@ -0,0 +1,27 @@
+__all__ = ["binomial_CI"]
+
+from .dist import Euc_cell2cell, Rad_cell2cell, Euc_syn2cell, Rad_syn2cell
+
+import numpy as np
+from caveclient import CAVEclient
+
+def binomial_CI(df, bins):
+    fail = []
+    success = []
+    # no cells are less than 0 microns away, and this makes sure my arrays are the same size
+    fail.append(0)
+    success.append(0)
+    for i in range(len(bins)-1):
+        # masking for specific distance bin
+        masked_df = df[(df['r'] < bins[i+1]) & (df['r'] > bins[i])].reset_index(drop=True)
+        # starting the counter
+        f,s = 0,0
+        for j in range(len(masked_df)):
+            if masked_df['pre_pt_root_id'][j] == 0:
+                f += 1
+            else:
+                s += 1
+        # if there are zero cells in masked_df, 0's are appended
+        fail.append(f)
+        success.append(s)
+    return np.array(fail),np.array(success)
